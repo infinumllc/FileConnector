48ef3cb39bda53751c0a109072557a5ac47abd2e
Daniel Bower <daniel.d.bower@gmail.com>
2012-10-30 13:42:21 -0400

    Remove database migration plugin for now as I seem to have something misconfigured

--- FileConnectorPluginGrailsPlugin.groovy
+++ FileConnectorPluginGrailsPlugin.groovy
@@ -4,7 +4,8 @@ class FileConnectorPluginGrailsPlugin {
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.1 > *"
     // the other plugins this plugin depends on
-    def dependsOn = ['database-migration':'1.1']
+	def dependsOn = [:]
+    //def dependsOn = ['database-migration':'1.1']
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"
